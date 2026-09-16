26151b64c8ffe578e07c23522a24a56f29049093
fermorales20020310-sys <fermorales20020310@gmail.com>
2026-09-16 15:06:18 -0500

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,9 @@
 import gracefulShutdown from "http-graceful-shutdown";
 import app from "./app";
-import { initIO } from "./libs/socket";
 import { logger } from "./utils/logger";
-import { initRedis } from "./libs/redisStore";
-import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
 
-const server = app.listen(process.env.PORT, () => {
-  logger.info(`Server started on port: ${process.env.PORT}`);
+const server = app.listen(process.env.PORT || 8080, () => {
+  logger.info(`Server started on port: ${process.env.PORT || 8080}`);
 });
 
-initIO(server);
-initRedis();
-StartAllWhatsAppsSessions();
 gracefulShutdown(server);
