416a0c826d50c3e610931fe633b62fa466742054
fermorales20020310-sys <fermorales20020310@gmail.com>
2026-09-16 11:33:07 -0500

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import { initRedis } from "./libs/redisStore";
 import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
 
 const server = app.listen(process.env.PORT, () => {
-  logger.info(`Server started on port: ${process.env.PORT}`);
+  logger.info(`Server started on port:${process.env.PORT}`);
 });
 
 initIO(server);
@@ -15,9 +15,9 @@ StartAllWhatsAppsSessions();
 gracefulShutdown(server);
 
 process.on("uncaughtException", err => {
-  logger.error({ info: "Global uncaught exception", err });
+  logger.error({ info: "global uncaught exception", err });
 });
 
 process.on("unhandledRejection", err => {
-  if (err) logger.error({ info: "Global unhandled rejection", err });
+  if (err) logger.error({ info: "global unhandled rejection", err });
 });
