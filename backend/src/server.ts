b09635bcb896fac460ac4b7f2f001bdf031c2dc4
fermorales20020310-sys <fermorales20020310@gmail.com>
2026-09-16 09:57:41 -0500

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,3 @@
-import gracefulShutdown from "http-graceful-shutdown";
-import app from "./app";
-import { initIO } from "./libs/socket";
-import { logger } from "./utils/logger";
-import { initRedis } from "./libs/redisStore";
-import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
-
-const server = app.listen(process.env.PORT, () => {
-  logger.info(`Server started on port: ${process.env.PORT}`);
-});
-
-initIO(server);
-initRedis();
-StartAllWhatsAppsSessions();
-gracefulShutdown(server);
-
-process.on("uncaughtException", err => {
-  logger.error({ info: "Global uncaught exception", err });
-});
-
-process.on("unhandledRejection", err => {
-  if (err) logger.error({ info: "Global unhandled rejection", err });
-});
+import app from './app';
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
